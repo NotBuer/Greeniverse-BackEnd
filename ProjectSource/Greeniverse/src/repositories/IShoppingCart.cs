5ffbe611d80fe16210cbc4243313005f4cd0249b
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-05-04 16:17:47 -0300

    @MuriloAGama [Task: Interface implementation] {04/05/2022 16:17}

--- ProjectSource/Greeniverse/src/repositories/IShoppingCart.cs
+++ ProjectSource/Greeniverse/src/repositories/IShoppingCart.cs
@@ -1,7 +1,17 @@
-﻿namespace Greeniverse.src.repositories.implementations
+﻿using Greeniverse.src.models;
+using System.Collections.Generic;
+
+namespace Greeniverse.src.repositories.implementations
 {
     public interface IShoppingCart
     {
+        void NewShoppingCart(NewShoppingCartDTO ShoppingCart);
+        void UpdateShoppingCart(UpdateShoppingCartDTO UpdateShoppingCart);
+        void DeleteShoppingCart(int id);
 
+        ShoppingCartModel GetShoppingCartById(int id);
+        ShoppingCartModel GetShoppingCartByPurchaseStatus(string purchaseStatus);
+        List<ShoppingCartModel> GetAllProducts();
+        
     }
 }
