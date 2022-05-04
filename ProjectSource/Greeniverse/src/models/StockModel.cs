2ca2a6dae26e07b08219eda88c836341871822b0
urielpereiraalves <upereiraalves@hotmail.com>
2022-05-04 15:25:09 -0300

    @urielpereiraalves[Task:StockModel implementation]{04/05/2022 -15:25}

--- ProjectSource/Greeniverse/src/models/StockModel.cs
+++ ProjectSource/Greeniverse/src/models/StockModel.cs
@@ -1,9 +1,43 @@
-﻿
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Greeniverse.src.models
 {
+    [Table("tb_stock")]
     public class StockModel
     {
-        
+        public StockModel() { }
+
+        public StockModel(int id, string type, string description, float price, string productName, string provider)
+        {
+            Id = id;
+            Type = type;
+            Description = description;
+            Price = price;
+            ProductName = productName;
+            Provider = provider;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Type { get; set; }
+
+        [Required, StringLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public float Price { get; set; }
+
+        [Required, StringLength(50)]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(50)]
+        public string Provider { get; set; }
+
     }
 }
