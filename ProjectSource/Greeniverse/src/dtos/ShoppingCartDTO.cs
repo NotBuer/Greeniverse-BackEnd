9ab7a99a2d78e8ae82826ec5b480eb225fe109f5
cateldev <catelmatt@hotmail.com>
2022-05-04 15:39:56 -0300

    @cateldev [Task: DTO implementation] {04/05/2022 15:40}

---
+++ ProjectSource/Greeniverse/src/dtos/ShoppingCartDTO.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Greeniverse.src.dtos
+{
+    public enum PaymentMethod { CreditCard, Paypal, PIX, Debit, CC }
+
+    public class NewShoppingCartDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int AmountProduct { get; set; }
+
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        public string Voucher { get; set; }
+
+        [Required, StringLength(50)]
+        public string DeliveryAdress { get; set; }
+
+        public NewShoppingCartDTO(int id, int amountProduct, PaymentMethod paymentMethod, string voucher, string deliveryAdress)
+        {
+            Id = id;
+            AmountProduct = amountProduct;
+            PaymentMethod = paymentMethod;
+            Voucher = voucher;
+            DeliveryAdress = deliveryAdress;
+        }
+    }
+    public class UpdateShoppingCartDTO
+    {
+        [Required]
+        public int AmountProduct { get; set; }
+
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        public string Voucher { get; set; }
+
+        [Required, StringLength(50)]
+        public string DeliveryAdress { get; set; }
+
+        public UpdateShoppingCartDTO(int amountProduct, PaymentMethod paymentMethod, string voucher, string deliveryAdress)
+        {
+            AmountProduct = amountProduct;
+            PaymentMethod = paymentMethod;
+            Voucher = voucher;
+            DeliveryAdress = deliveryAdress;
+        }
+    }
+}
