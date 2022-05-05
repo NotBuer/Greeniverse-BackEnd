4815cf12d61436348b06a75d55ab9c9fc614a9f0
GabrielaPeres <gabrieladperes1998@gmail.com>
2022-05-05 12:08:28 -0300

    @GabrielaPeres [Task:Implementation method ] {05/05/2022 12:08}

--- ProjectSource/Greeniverse/src/repositories/implementations/ShoppingCartRepository.cs
+++ ProjectSource/Greeniverse/src/repositories/implementations/ShoppingCartRepository.cs
@@ -1,7 +1,26 @@
-﻿namespace Greeniverse.src.repositories.implementations
+﻿using Greeniverse.src.data;
+using Greeniverse.src.dtos;
+using Greeniverse.src.models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Greeniverse.src.repositories.implementations
 {
-    public class ShoppingCartRepository
+    public class ShoppingCartRepository : IShoppingCart
     {
-        
-    }
-}
+        #region Attribute
+        private readonly GreeniverseContext _context;
+        #endregion Attribute
+
+        #region Constructor
+        public ShoppingCartRepository(GreeniverseContext context)
+        {
+            _context = context;
+        }
+        #endregion Constructor
+        #region Methods
+        public void DeleteShoppingCart(int id)
+        {
+            _context.ShoppingCart.Remove(GetShoppingCartById(id));
+            _context.SaveChanges();
+        }
