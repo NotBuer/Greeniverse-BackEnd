2a234fa12bf97e0cb46528982870c30c8f572fd9
Rodrigo F. (NotBuer) <rodrigosoocer@gmail.com>
2022-05-05 10:55:27 -0300

    @NotBuer[Task: UserRepository.cs Schema] {05/05/2022 - 10:55}

--- ProjectSource/Greeniverse/src/repositories/implementations/UserRepository.cs
+++ ProjectSource/Greeniverse/src/repositories/implementations/UserRepository.cs
@@ -1,7 +1,62 @@
-﻿namespace Greeniverse.src.repositories.implementations
+﻿using Greeniverse.src.data;
+using Greeniverse.src.DTOS;
+using Greeniverse.src.models;
+using System.Collections.Generic;
+
+namespace Greeniverse.src.repositories.implementations
 {
-    public class UserRepository
+
+    public class UserRepository : IUser
     {
+
+        #region ATTRIBUTES
+        private readonly GreeniverseContext _context;
+        #endregion
+
         
+        #region CONSTRUCTORS
+        public UserRepository(GreeniverseContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+
+        #region METHODS
+        public void NewUser(NewUserDTO user)
+        {
+
+        }
+
+        public void UpdateUser(UpdateUserDTO user)
+        {
+
+        }
+
+        public void DeleteUser(int id)
+        {
+            
+        }
+
+        public UserModel GetUserByEmail(string email)
+        {
+            // TODO: Need implementation.
+            return null;
+        }
+
+        public UserModel GetUserById(int id)
+        {
+            // TODO: Need implementation.
+            return null;
+        }
+
+        public List<UserModel> GetUserByName(string name)
+        {
+            // TODO: Need implementation.
+            return null;
+        }
+        #endregion
+
     }
+
 }
