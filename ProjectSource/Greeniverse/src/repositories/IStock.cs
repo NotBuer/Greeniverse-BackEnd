22a97af680d3dcc71c721bd077288dc7d40cf769
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-05-05 11:22:11 -0300

    @MuriloAGama [Task: Create schema StockRepository] {05/05/2022 11:22}

--- ProjectSource/Greeniverse/src/repositories/IStock.cs
+++ ProjectSource/Greeniverse/src/repositories/IStock.cs
@@ -1,12 +1,13 @@
-﻿using Greeniverse.src.models;
+﻿using Greeniverse.src.dtos;
+using Greeniverse.src.models;
 using System.Collections.Generic;
 
 namespace Greeniverse.src.repositories.implementations
 {
     public interface IStock
     {
-        void NewProduct(NewProductDTO Product);
-        void UpdateProduct(UpdateProductDTO Updateproduct);
+        void NewProduct(NewStockDTO Product);
+        void UpdateProduct(UpdateStockDTO Updateproduct);
         void DeleteProduct(int id);
         StockModel GetProductById(int id);
         List<StockModel> GetAllProducts();
--- ProjectSource/Greeniverse/src/repositories/implementations/StockRepository.cs
+++ ProjectSource/Greeniverse/src/repositories/implementations/StockRepository.cs
@@ -1,7 +1,45 @@
-﻿namespace Greeniverse.src.repositories.implementations
+﻿using Greeniverse.src.data;
+using Greeniverse.src.dtos;
+using Greeniverse.src.models;
+using System.Collections.Generic;
+
+namespace Greeniverse.src.repositories.implementations
 {
-    public class StockRepository
+    public class StockRepository :IStock
     {
-        
+        #region Attributes
+
+        private readonly GreeniverseContext _context;
+
+        #endregion Attributes
+
+        #region Builders
+
+        public StockRepository(GreeniverseContext context)
+        {
+            _context = context;
+        }
+
+        #endregion Builders
+
+        #region Method
+
+        public void NewProduct(NewStockDTO Product)
+        {
+            
+        }
+
+        public void DeleteProduct(int id)
+        {
+
+        }
+
+        public List<StockModel> GetAllProducts()
+        {
+
+        }
+
     }
+
+    #endregion Method
 }
