adeedb3d010debc78b02dc7563ab9735fd19138a
cateldev <catelmatt@hotmail.com>
2022-05-09 15:16:13 -0300

    @cateldev [Stock Controller] {09/05/2022}

--- ProjectSource/Greeniverse/src/controllers/StockController.cs
+++ ProjectSource/Greeniverse/src/controllers/StockController.cs
@@ -1,14 +1,35 @@
-﻿using Greeniverse.src.dtos;
-using Greeniverse.src.repositories.implementations;
-using Microsoft.AspNetCore.Mvc;
+﻿
+        [HttpGet("search")]
+        public IActionResult GetProductsBySearch(
+            [FromQuery] string type,
+            [FromQuery] string description,
+            [FromQuery] string productName)
+        {
+            var stocks = _repository.GetProductsBySearch(type, description, productName);
+            if (stocks.Count < 1) return NoContent();
+            return Ok(stocks);
+        }
+        [HttpPost]
+        public IActionResult NewProduct([FromBody] NewStockDTO stock)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            _repository.NewProduct(stock);
+            return Created($"Api/stocks/id/{stock.Id}", stock);
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct([FromBody] UpdateStockDTO stock)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            _repository.UpdateProduct(stock);
+            return Ok(stock);
+        }
 
-namespace Greeniverse.src.controllers
-{
-    [ApiController]
-    [Route("api/Stock")]
-    [Produces("application/json")]
-    public class StockControllers : ControllerBase
-    {
-        #region Atributos
-        private readonly IStock _repository;
+        [HttpDelete(("delete/{idStock}"))]
+        public IActionResult DeleteProduct([FromRoute] int idStock)
+        {
+            _repository.DeleteProduct(idStock);
+            return Ok();
+        }
         #endregion
+    }
+}
