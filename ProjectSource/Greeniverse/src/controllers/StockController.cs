81ee5242d4660ce27199301eb3058a84826f33d5
cateldev <catelmatt@hotmail.com>
2022-05-09 15:12:01 -0300

    @cateldev [Stock Controller] {09/05/2022}

--- ProjectSource/Greeniverse/src/controllers/StockController.cs
+++ ProjectSource/Greeniverse/src/controllers/StockController.cs
@@ -1,6 +1,14 @@
-﻿namespace Greeniverse.src.controllers
+﻿using Greeniverse.src.dtos;
+using Greeniverse.src.repositories.implementations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greeniverse.src.controllers
 {
-    public class StockController
+    [ApiController]
+    [Route("api/Stock")]
+    [Produces("application/json")]
+    public class StockControllers : ControllerBase
     {
-    }
-}
+        #region Atributos
+        private readonly IStock _repository;
+        #endregion
