39fd07a349266317dc8f3e900951bcad04b56c5e
cateldev <catelmatt@hotmail.com>
2022-05-11 16:14:46 -0300

    @cateldev [Task: Methods Implantation] {11/05/2022}

--- ProjectSource/Greeniverse/src/services/implementations/AuthenticationServices.cs
+++ ProjectSource/Greeniverse/src/services/implementations/AuthenticationServices.cs
@@ -1,6 +1,10 @@
 ﻿namespace Greeniverse.src.services.implementations
 {
-    public class AuthenticationServices
+    public void CreateUserNoDuplicate(NewUserDTO dto)
     {
+        var user = _repository.GetUserByEmail(dto.Email);
+        if (user != null) throw new Exception("Este email já está sendo utilizado");
+        dto.Password = CodifyPassword(dto.Password);
+        _repository.NewUser(dto);
     }
 }
---
+++ ProjectSource/TestEnvironment/Tests/repositories/StockRepositoryTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using BlogPessoal.src.data;
+using BlogPessoal.src.dtos;
+using BlogPessoal.src.repositorios;
+using BlogPessoal.src.repositorios.implementacoes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestMethod]
+    public async Task GetStockByDescriptionReturnProductName()
+    {   
+        
+        var opt = new DbContextOptionsBuilder<GreeniverseContext>()
+        .UseInMemoryDatabase(databaseName: "db_blogpessoal12")
+        .Options;
+
+        _contexto = new BlogPessoalContexto(opt);
+        _repositorio = new TemaRepositorio(_contexto);
+
+        
+        await _repositorio.NovoTemaAsync(new NovoTemaDTO("Java"));
+     
+        await _repositorio.NovoTemaAsync(new NovoTemaDTO("JavaScript"));
+
+      
+        var temas = await _repositorio.PegarTemasPelaDescricaoAsync("Java");
+
+     
+        Assert.AreEqual(2, temas.Count);
+    }
+
+    [TestMethod]
+    public async Task AlterarTemaPythonRetornaTemaCobol()
+    {
+        
+        var opt = new DbContextOptionsBuilder<BlogPessoalContexto>()
+        .UseInMemoryDatabase(databaseName: "db_blogpessoal13")
+        .Options;
+
+        _contexto = new BlogPessoalContexto(opt);
+        _repositorio = new TemaRepositorio(_contexto);
+
+       
+        await _repositorio.NovoTemaAsync(new NovoTemaDTO("Python"));
+
+        await _repositorio.AtualizarTemaAsync(new AtualizarTemaDTO(1, "COBOL"));
+
+        var stock = await _repository.GetProductByIdAsync(1);
+
+        Assert.AreEqual("COBOL", stock.Description);
+    }
