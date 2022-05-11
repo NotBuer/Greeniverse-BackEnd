bc411ba41829566aefba9befd2b3f4dbbd143aa8
cateldev <catelmatt@hotmail.com>
2022-05-11 20:22:17 -0300

    @cateldev [Task: Protecting Methods] {11/05/2022 20:30}

--- ProjectSource/Greeniverse/src/dtos/AuthenticationDTO.cs
+++ ProjectSource/Greeniverse/src/dtos/AuthenticationDTO.cs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-﻿namespace Greeniverse.src.dtos{    public class AuthenticationDTO    {    }}
-=======
-﻿using System.ComponentModel.DataAnnotations;
+using Greeniverse.src.utils;using System.ComponentModel.DataAnnotations;namespace Greeniverse.src.dtos{    public class AuthenticationDTO    {        [Required]        public string Email { get; set; }        [Required]        public string Password { get; set; }        public AuthenticationDTO(string email, string password)        {            Email = email;            Password = password;        }    }    public class AuthorizationDTO    {        public int Id { get; set; }        public string Email { get; set; }        public UserType Type { get; set; }        public string Token { get; set; }        public AuthorizationDTO(int id, string email, UserType type, string token)        {            Id = id;            Email = email;            Type = type;            Token = token;        }    }}
 
-namespace Greeniverse.src.dtos
-{
-    public class AuthenticationDTO
-    {
-        [Required]
-        public string Email { get; set; }
 
-        [Required]
-        public string Password { get; set; }
-        public AuthenticationDTO(string email, string password)
-        {
-            Email = email;
-            Password = password;
-        }
-    }
-}
->>>>>>> fc74dd963581509c51fa502658772a0aa7647f97
