1b7259733a39607971e7b8ca55af21aaf5eb276a
cateldev <catelmatt@hotmail.com>
2022-05-11 19:45:35 -0300

    @cateldev [Task: Authentication Controller {11/05/2022 19:45}

--- ProjectSource/Greeniverse/src/controllers/AuthenticationController.cs
+++ ProjectSource/Greeniverse/src/controllers/AuthenticationController.cs
@@ -1,6 +1,44 @@
-﻿namespace Greeniverse.src.controllers
+﻿using System;
+using Greeniverse.src.dtos;
+using Greeniverse.src.services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPessoal.src.controladores
 {
-    public class AuthenticationController
+    [ApiController]
+    [Route("api/Authentication")]
+    [Produces("application/json")]
+
+    public class AuthenticationController : ControllerBase
     {
+        #region Attribute
+        private readonly IAuthentication _services;
+        #endregion Attribute
+
+        #region Constructor
+        public AuthenticationController(IAuthentication services)
+        {
+            _services = services;
+        }
+        #endregion Constructor
+
+        #region Methods
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Authenticate([FromBody] AuthenticationDTO authentication)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            try
+            {
+                var authorization = _services.GetAuthorization(authentication);
+                return Ok(authorization);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+        #endregion
     }
 }
