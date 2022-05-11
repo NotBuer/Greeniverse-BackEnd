20653378d477827a2a4d0cd121862907cfda5148
kaufariax <eikauanefarias@gmail.com>
2022-05-11 15:59:20 -0300

    @kaufariax [Task: Implementing AuthorizationDTO]{11/05/2022 16:00}

--- ProjectSource/Greeniverse/src/dtos/AuthenticationDTO.cs
+++ ProjectSource/Greeniverse/src/dtos/AuthenticationDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Greeniverse.src.utils;
+using System.ComponentModel.DataAnnotations;
 
 namespace Greeniverse.src.dtos
 {
@@ -9,10 +10,26 @@ namespace Greeniverse.src.dtos
 
         [Required]
         public string Password { get; set; }
+
         public AuthenticationDTO(string email, string password)
         {
             Email = email;
             Password = password;
+        }
+    }
+    public class AuthorizationDTO
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public UserType Type { get; set; }
+        public string Token { get; set;  }
+
+        public AuthorizationDTO(int id, string email, UserType type, string token)
+        {
+            Id = id;
+            Email = email;
+            Type = type;
+            Token = token;
         }
     }
 }
