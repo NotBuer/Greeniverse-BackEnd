bc2359237b149b0e96085a0bc4ea27fdd308823d
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-05-11 15:46:01 -0300

    @MuriloAGama [Task: Implementing AuthenticationDTO] {11/05/2022 15:44}

--- ProjectSource/Greeniverse/src/dtos/AuthenticationDTO.cs
+++ ProjectSource/Greeniverse/src/dtos/AuthenticationDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace Greeniverse.src.dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Greeniverse.src.dtos
 {
     public class AuthenticationDTO
     {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        public AuthenticationDTO(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
     }
 }
