8817eac3bb87918d092c30983fd18177e55ed95f
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-05-12 15:40:03 -0300

    @MuriloAGama [Task: initializing test shoppingCartRepository] {12/05/2022 15:39}

---
+++ ProjectSource/TestEnvironment/Tests/repositories/ShoppingCartRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using Greeniverse.src.data;
+using Greeniverse.src.dtos;
+using Greeniverse.src.repositories.implementations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEnvironment.Tests.repositories
+{
+    [TestClass]
+    public class ShoppingCartRepositoryTest
+    {
+        private GreeniverseContext _context;
+        private IUser _repositoryU;
+        private IStock _repositoryS;
+        private IShoppingCart _repositorySC;
+
+    [TestMethod]
+    _repositorySC.UpdateShoppingCart(
+        new UpdateShoppingCartDTO(
+            1,
+            15,
+            "Pix",
+            "desconto20reais",
+            "Rua forte de tamandare, 556"
+            )
+};
