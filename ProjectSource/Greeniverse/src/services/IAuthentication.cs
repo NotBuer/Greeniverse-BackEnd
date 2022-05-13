930ed3cfbd2dde603704a1aa96aaa545b5696c02
kaufariax <eikauanefarias@gmail.com>
2022-05-13 17:29:23 -0300

    @kaufariax [Task: Implementing Documentation]{13/05/2022 17:30}

--- ProjectSource/Greeniverse/src/services/IAuthentication.cs
+++ ProjectSource/Greeniverse/src/services/IAuthentication.cs
@@ -5,16 +5,16 @@ using System.Threading.Tasks;
 
 namespace Greeniverse.src.services
 {
+    /// <summary>
+    /// <para>Resume: Interface Responsible for representing authentication actions</para>
+    /// <para>Version: 1.0</para>
+    /// <para>Data: 2022-05-03</para>
+    /// </summary>
     public interface IAuthentication
     {
         string EncodePassword (string password);
         Task CreateUserWithoutDuplicateAsync(NewUserDTO user);
         string GenerateToken(UserModel user);
-<<<<<<< HEAD:ProjectSource/Greeniverse/src/services/implementations/IAuthentication.cs
-        AuthorizationDTO GetAuthorization(AuthenticationDTO authentication);
-        Task CreateUserWithoutDuplicateAsync(NewUserDTO user);
-=======
         Task<AuthorizationDTO> GetAuthorizationAsync(AuthenticationDTO authentication);
->>>>>>> 89007d78768a4e816a61ef7c2ba4ddd7ed3e1b99:ProjectSource/Greeniverse/src/services/IAuthentication.cs
     }
 }
