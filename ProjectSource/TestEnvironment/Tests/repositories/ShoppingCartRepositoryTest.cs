e99192c6c4eab2a94c107c8c316e825ca3793bfc
kaufariax <eikauanefarias@gmail.com>
2022-05-19 20:53:52 -0300

    @kaufariax [Task: Implementing corrections]{19/05/2022 23:55}

--- ProjectSource/TestEnvironment/Tests/repositories/ShoppingCartRepositoryTest.cs
+++ ProjectSource/TestEnvironment/Tests/repositories/ShoppingCartRepositoryTest.cs
@@ -1,6 +1,8 @@
 ﻿using Greeniverse.src.data;
 using Greeniverse.src.dtos;
 using Greeniverse.src.repositories.implementations;
+using Greeniverse.src.utils;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -20,20 +22,54 @@ namespace TestEnvironment.Tests.repositories
         private IShoppingCart _repositorySC;
 
         [TestMethod]
-        private void CreateThreeShoppingCartsReturnsThreeShoppingCartNotNull()
+        private async Task CreateShoppingCartsReturnsShoppingCartNotNull()
         {
+            var opt = new DbContextOptionsBuilder<GreeniverseContext>()
+                .UseInMemoryDatabase(databaseName: "db_greeniverse1")
+                .Options;
+
+            _context = new GreeniverseContext(opt);
+            _repositorySC = new ShoppingCartRepository(_context);
+
+            await _repositorySC.NewShoppingCartAsync(
+                  new NewShoppingCartDTO(
+                      10, PaymentMethod.PIX, "50% de Desconto", "AddressTest", "murilinho@gmail.com", 1)
+              );
+
+            Assert.AreEqual(1, _context.ShoppingCart.Count());
+        }
+
+        [TestMethod]
+        private async Task GetAllProductsByEmailPurchaserAsyncReturnsNotNull()
+        {
+            var opt = new DbContextOptionsBuilder<GreeniverseContext>()
+                .UseInMemoryDatabase(databaseName: "db_greeniverse1")
+                .Options;
+
+            _context = new GreeniverseContext(opt);
+            _repositorySC = new ShoppingCartRepository(_context);
+
+            await _repositorySC.NewShoppingCartAsync(
+                 new NewShoppingCartDTO(
+                     5, PaymentMethod.PIX, "10% de Desconto", "AddressTest", "catel@gmail.com", 2)
+             );
 
         }
 
         [TestMethod]
-        private void GetCartByDeliveryAddressReturnsNotNull()
+        private async Task GetCartByIdReturnNotNullAndPaymentMethod()
         {
+            var opt = new DbContextOptionsBuilder<GreeniverseContext>()
+                .UseInMemoryDatabase(databaseName: "db_greeniverse1")
+                .Options;
 
-        }
+            _context = new GreeniverseContext(opt);
+            _repositorySC = new ShoppingCartRepository(_context);
 
-        [TestMethod]
-        private void GetCartByIdReturnNotNullAndPaymentMethod()
-        {
+            await _repositorySC.NewShoppingCartAsync(
+                new NewShoppingCartDTO(
+                    12, PaymentMethod.CreditCard, "25% de Desconto", "AddressTest", "kauzinha@gmail.com", 5)
+            );
 
         }
 
